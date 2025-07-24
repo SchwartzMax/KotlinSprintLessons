5f9aff8500214af09ed6fb9dc3e7a0a2f965637e
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-24 23:40:15 +0300

    first

--- src/main/kotlin/lesson1/taks3.kt
+++ src/main/kotlin/lesson1/taks3.kt
@@ -5,9 +5,9 @@ fun main() {
     var hour = 9
     var minute = 7
 
-    print("Год полета - $year ")
-    print("Час - 0$hour ")
-    println("минуты - 0$minute ")
+    print("Год полета - ${year.toString()} ")
+    print("Час - 0${hour.toString()} ")
+    println("минуты - 0${minute.toString()} ")
 
     hour = 10
     minute = 55
