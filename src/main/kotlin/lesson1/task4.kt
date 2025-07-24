0581374f01d78cc9988e21ddded55f83d070c2a1
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-24 23:45:22 +0300

    first

--- src/main/kotlin/lesson1/task4.kt
+++ src/main/kotlin/lesson1/task4.kt
@@ -4,8 +4,8 @@ fun main() {
     val distance: Long = 40868600000
     val age: Byte = 27
     val partOfTheDay: Float = 0.075F
-    val second: Int = 6480
-    val partOfTheYear:Double = 2.0547945205479453E-4
+    val second: Short = 6480
+    val partOfTheYear: Float = 2.0547945205479453E-4F
     val apogeeOfTheOrbit: Int = 327000
 
     println(distance)
