7c7ebdd7e4f7ca8b931a6fd791323f5773df51b6
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-24 20:50:55 +0300

    first

---
+++ src/main/kotlin/lesson1/taks2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson1
+
+fun main(){
+    val numberOfOrders: Int = 75
+    val ThanksForAThePurchase = "Спасибо за покупку!"
+    println("Колличество заказов = $numberOfOrders")
+    println(ThanksForAThePurchase)
+
+    var numberOfEmployees: Int = 2000
+//    println("Колличество работников = $numberOfEmployees")
+
+    numberOfEmployees = 1999
+    println("Колличество работников = $numberOfEmployees") // один работник уволился
+}
