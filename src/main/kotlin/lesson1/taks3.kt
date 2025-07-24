1dac011e9d2f21bf1a8be5365d42936ca346ef19
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-24 21:42:49 +0300

    first

---
+++ src/main/kotlin/lesson1/taks3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    print("Год полета - $year ")
+    print("Час - 0$hour ")
+    println("минуты - 0$minute ")
+
+    hour = 10
+    minute = 55
+
+    println("Посадка - $hour:$minute")
+
+}
