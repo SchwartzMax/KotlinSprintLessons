042312d7401fdd2b1281b8f5122f40a16fdce92d
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-25 16:48:33 +0300

    Taks 2 Accounting software

---
+++ src/main/kotlin/lesson1/task3.kt
@@ -0,0 +1,15 @@
+package lesson1
+
+fun main() {
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+
+    print("Год полета - $year ")
+    print("Час - $hour")
+    println("минуты - $minute")
+
+    hour = "10"
+    minute = "55"
+    println("Посадка - $hour:$minute")
+}
