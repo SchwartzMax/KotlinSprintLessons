41c126acaf3e308df206c05e4014fca925089b94
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-25 14:46:22 +0300

    Taks 1 Calculating the arithmetic mean

---
+++ src/main/kotlin/lesson2/task1.kt
@@ -0,0 +1,10 @@
+package lesson2
+
+fun main() {
+    val student1 = 3.00f
+    val student2 = 4.00f
+    val student3 = 3.00f
+    val student4 = 5.00f
+
+    println((student1 + student2 + student3 + student4) / 4)
+}
