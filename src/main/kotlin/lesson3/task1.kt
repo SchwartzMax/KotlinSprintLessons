ca4b7972debc25ff1a06a2a6a362e0f7a1a36ecd
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-26 16:07:32 +0300

    KS-3-1 WelcomeText

--- src/main/kotlin/lesson3/task1.kt
+++ src/main/kotlin/lesson3/task1.kt
@@ -1,12 +1,12 @@
 package lesson3
 
 fun main() {
-    val helloDay = "Good afternoon! "
-    val helloEvening = "Good evening! "
+    val helloDay = "Good afternoon"
+    val helloEvening = "Good evening"
     val name = "Maxim"
 
-    println(helloDay + name)
-    println(helloEvening + name)
+    println("$helloDay $name")
+    println("$helloEvening $name")
 
 }
 
