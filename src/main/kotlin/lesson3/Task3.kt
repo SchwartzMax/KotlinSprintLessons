6241f73f921d3f66b43e16c3bf563169252fcbf3
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-26 14:03:07 +0300

    KS-2-4-PercentageCalculation

--- src/main/kotlin/lesson3/Task3.kt
+++ src/main/kotlin/lesson3/Task3.kt
@@ -1,12 +1,17 @@
 package lesson3
+const val minutes: Int = 60
 
 fun main() {
     val hourStart = 9
     val minuteStart = 39
-    val OnTheWay = 457
+    val onTheWay = 457
 
-    var hourFinish = hourStart + (OnTheWay / 60)
-    var minuteFinish = minuteStart + (minuteStart % 60)
 
-    println("Время прибытия ${hourFinish+1}:${minuteFinish-60}")
+    var hourFinish = hourStart + (onTheWay /minutes)
+    var minuteFinish = minuteStart + (minuteStart % minutes )
+
+    var result1 = hourFinish+1
+    var result2 = minuteFinish-minutes
+
+    println("Время прибытия $result1:$result2")
 }
