cf4442cbdd18c91dcf3a3134d318786007baab39
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-27 20:42:26 +0300

    KS-1-4 DifferenetTypesOfVariables

--- src/main/kotlin/lesson1/task4.kt
+++ src/main/kotlin/lesson1/task4.kt
@@ -4,15 +4,14 @@ fun main() {
     val distance: Long = 40868600000
     val age: Byte = 27
     val partOfTheDay: Float = 0.075F
-    val second: Short = 6480
-    val partOfTheYear: Float = 2.0547945205479453E-4F
+    val seconds: Short = 6480
+    val partOfTheYear: Double = 2.0547945205479453E-4
     val apogeeOfTheOrbit: Int = 327000
-
 
     println(distance)
     println(age)
     println(partOfTheDay)
-    println(second)
+    println(seconds)
     println(partOfTheYear)
     println(apogeeOfTheOrbit)
 
