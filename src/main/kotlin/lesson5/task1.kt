dd339c40f662b02aa89d7ddc2a7dff0504461ebc
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-31 17:06:34 +0300

    KS-5-1 loginVerification

---
+++ src/main/kotlin/lesson5/task1.kt
@@ -0,0 +1,11 @@
+package lesson5
+
+fun main() {
+    println("Сложите два числа и напишите ответ - 4+3 ")
+    var result = readln().toInt()
+    if (result == 7)
+        println("Добро пожаловать!")
+    else
+        println("Доступ запрещен")
+
+}
