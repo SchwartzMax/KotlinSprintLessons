c28c7c3a3836cfd1feee9d313e6f2f63bba68d48
SchwartzMax <maks.chernenko.2019@mail.ru>
2025-07-31 18:15:50 +0300

    KS-4-3 CheckingTheGrowthConditions

--- src/main/kotlin/lesson4/task3.kt
+++ src/main/kotlin/lesson4/task3.kt
@@ -2,9 +2,8 @@ package lesson4
 
 const val PERCENTAGE_HUMIDITY = 20
 const val UNFAVORABLE_SEASON = "winter"
-const val SUNNY_DAY = true
-const val OPEN_WINDOW = true
-
+const val IS_SUNNY_DAY = true
+const val IS_WINDOW_OPEN = true
 
 fun main() {
     val isSunny: Boolean = true
@@ -13,6 +12,6 @@ fun main() {
     val currentTimeOfTheYear: String = "winter"
 
     val conditions: Boolean =
-        (isSunny == SUNNY_DAY) && (isAwningIsOpen == OPEN_WINDOW) && (airHumidity == PERCENTAGE_HUMIDITY) && (currentTimeOfTheYear != UNFAVORABLE_SEASON)
+        (isSunny == IS_SUNNY_DAY) && (isAwningIsOpen == IS_WINDOW_OPEN) && (airHumidity == PERCENTAGE_HUMIDITY) && (currentTimeOfTheYear != UNFAVORABLE_SEASON)
     println("Благоприятные ли условия сейчас для роста бобовых? $conditions")
 }
